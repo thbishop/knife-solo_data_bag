789fd79f441fe6caeff2b2c0744a8418f69ec6aa
Tommy Bishop <bishop.thomas@gmail.com>
2012-11-08 17:10:07 -0800

    tweak debug output when overriding knife.rb secret

--- lib/chef/knife/helpers.rb
+++ lib/chef/knife/helpers.rb
@@ -61,7 +61,7 @@ module KnifeSoloDataBag
         ui.fatal 'Please specify either --secret or --secret-file only'
         exit 1
       elsif (config[:secret] && secret_path) || (config[:secret_file] && secret_path)
-        ui.info 'NOTE: The encrypted_data_bag_secret option defined in knife.rb was overriden by --secret-file.'
+        ui.debug 'NOTE: The encrypted_data_bag_secret option defined in knife.rb was overriden by the command line.'
       end
     end
 
