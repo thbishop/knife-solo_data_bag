28d059f434852963a7d1d543468382ab57c491cd
A.S. Lomoff <lomov.as@gmail.com>
2013-12-01 20:49:12 +0300

    supply necessary instance vars to SoloDataBagEdit

--- lib/chef/solo_data_bag_item.rb
+++ lib/chef/solo_data_bag_item.rb
@@ -1,7 +1,11 @@
 class Chef::DataBagItem
 
   def save
-    KnifeSoloDataBag::SoloDataBagEdit.new([self.data_bag, self.name]).send(:update_bag_item, self.to_json)
+    Chef::Config[:solo] = true
+    data_bag_item = KnifeSoloDataBag::SoloDataBagEdit.new([self.data_bag, self.name])
+    data_bag_item.instance_variable_set :@bag_name, self.data_bag
+    data_bag_item.instance_variable_set :@item_name, self.name
+    data_bag_item.send(:update_bag_item, self.to_hash)
   end
 
 end
