689b425fb21033c703e8a0bdab3a84698b5876f0
Tommy Bishop <bishop.thomas@gmail.com>
2014-02-18 16:07:32 -0800

    minor integration script cleanup

--- test/integration/bootstrap.sh
+++ test/integration/bootstrap.sh
@@ -2,16 +2,18 @@
 
 set -e
 
+DATA_DIR=/tmp/kitchen/data
+
 function download_chef_version() {
   echo -e "Installing curl..."
   yum install -y curl
   echo "Done"
 
   echo -e "Downloading chef install script..."
-  curl -o /tmp/kitchen/data/chef_install.sh https://www.opscode.com/chef/install.sh
-  chmod +x /tmp/kitchen/data/chef_install.sh
+  curl -o $DATA_DIR/chef_install.sh https://www.opscode.com/chef/install.sh
+  chmod +x $DATA_DIR/chef_install.sh
   echo "Done"
-  /tmp/kitchen/data/chef_install.sh -v $1
+  $DATA_DIR/chef_install.sh -v $1
 }
 
 for version in 11.2.0-1 11.4.4-2 11.6.2-1 11.8.2-1 11.10.0-1; do
@@ -19,21 +21,21 @@ for version in 11.2.0-1 11.4.4-2 11.6.2-1 11.8.2-1 11.10.0-1; do
   echo "# Processing $version"
   echo "##############################"
 
-  if [ ! -f /tmp/kitchen/data/packages/chef-$version.el6.x86_64.rpm ]; then
+  if [ ! -f $DATA_DIR/packages/chef-$version.el6.x86_64.rpm ]; then
     download_chef_version $version
   else
-    rpm -ivh --quiet /tmp/kitchen/data/packages/chef-$version*.rpm
+    rpm -ivh --quiet $DATA_DIR/packages/chef-$version*.rpm
   fi
 
   echo -e "Installing knife-solo_data_bag..."
-  /opt/chef/embedded/bin/gem install -q /tmp/kitchen/data/packages/*1.0.0.gem --no-ri --no-rdoc
+  /opt/chef/embedded/bin/gem install -q $DATA_DIR/packages/*.gem --no-ri --no-rdoc
   echo "Done"
 
-  knife solo data bag create foo bar --data-bag-path /tmp/kitchen/data/data_bags -j '{ "id": "bar", "my": "data" }'
+  knife solo data bag create foo bar --data-bag-path $DATA_DIR/data_bags -j '{ "id": "bar", "my": "data" }'
 
-  chef-solo -c /tmp/kitchen/data/solo.rb -o foo -l info
+  chef-solo -c $DATA_DIR/solo.rb -o foo -l info
 
-  knife solo data bag show foo bar --data-bag-path /tmp/kitchen/data/data_bags
+  knife solo data bag show foo bar --data-bag-path $DATA_DIR/data_bags
 
   echo -e "Cleaning up..."
   rpm -e --quiet chef
