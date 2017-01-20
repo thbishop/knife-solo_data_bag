cf2bd23c3ea0df55e5de18b60d31cc87ab899658
troyready <troy@troyready.com>
2017-01-20 14:39:45 -0800

    Enable solo_legacy_mode for for Chef >= 12.11
    Adapted fix from #37 in a backwards-compatible version.

--- lib/chef/knife/solo_data_bag_edit.rb
+++ lib/chef/knife/solo_data_bag_edit.rb
@@ -29,6 +29,7 @@ class Chef
 
       def run
         Chef::Config[:solo]   = true
+        Chef::Config[:solo_legacy_mode] = true if Gem.loaded_specs['chef'].version > Gem::Version.new('12.11.0')
         @bag_name, @item_name = @name_args
         ensure_valid_arguments
         edit_content
